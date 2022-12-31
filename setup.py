2d698dc5b04b453f8f94947908e6433e6251e99a
big_dick_for_putin@protonmail.com <apeks37@yandex.ru>
2022-12-30 22:13:13 -0500

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="bilat",
+    version="0.0.1",
+    author="TiredFingers",
+    author_email="tiredfingers@yandex.ru",
+    description="Technical Analyse Lib",
+    packages=setuptools.find_packages(),
+)
